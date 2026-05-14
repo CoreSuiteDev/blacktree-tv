6752c58effa7666a23d1e1a7e26b45d748901b1f
ahamedasif07 <rxasif31@gmail.com>
2026-05-14 09:47:28 +0600

    interview page done

--- web/app/(public)/interviews/page.tsx
+++ web/app/(public)/interviews/page.tsx
@@ -1,11 +1,13 @@
 import { ExclusiveInterview } from "@/components/shared/home/SpotlightSlider";
 import { INTERVIEW_SPORTLIGHT } from "@/constants/movies";
 import React from "react";
+import InterviewsSection from "./_components/interviews-section";
 
 const Interview = () => {
   return (
     <div className="mt-14 px-4 md:px-0">
       <ExclusiveInterview items={INTERVIEW_SPORTLIGHT} />
+      <InterviewsSection />
     </div>
   );
 };
