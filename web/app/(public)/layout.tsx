75712d310ccdf6a54916222a8f8cce926788816f
masrafi-000 <smmasrafi000@gmail.com>
2026-05-21 16:05:35 +0600

    feat: add layout for public pages including navbar and footer components

--- web/app/(public)/layout.tsx
+++ web/app/(public)/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/shared/footer/Footer";
-import { Navbar } from "@/components/shared/navbar/navbar";
+import { Navbar } from "@/components/shared/navbar/Navbar";
 
 export default function PublicLayout({
   children,
